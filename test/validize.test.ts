a312288a03ea6bd55270290d05c7a13d8f04f666
Jared Krinke <jared.krinke@gmail.com>
2020-01-05 07:48:59 -0800

    Group tests

--- test/validize.test.ts
+++ test/validize.test.ts
@@ -3,81 +3,89 @@ import * as assert from "assert";
 import * as Validize from "../src/validize";
 
 describe("Validize", () => {
-    it("String validation", () => {
-        const validate = Validize.createStringValidator(/^[a-z]*$/i);
-        assert.equal(validate("abc"), "abc");
-        assert.equal(validate("aBc"), "aBc");
-        assert.throws(() => { validate("aB1c") });
+    describe("String validation", () => {
+        it("Pattern", () => {
+            const validate = Validize.createStringValidator(/^[a-z]*$/i);
+            assert.equal(validate("abc"), "abc");
+            assert.equal(validate("aBc"), "aBc");
+            assert.throws(() => { validate("aB1c") });
 
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate(3) });
-        assert.throws(() => { validate({}) });
-        assert.throws(() => { validate(true) });
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate(3) });
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate(true) });
+        });
     });
 
-    it("Integer validation", () => {
-        const validate = Validize.createIntegerValidator(1, 3);
-        assert.equal(validate(1), 1);
-        assert.equal(validate(3), 3);
+    describe("Integer validation", () => {
+        it("Exact", () => {
+            const validate = Validize.createIntegerValidator(1, 3);
+            assert.equal(validate(1), 1);
+            assert.equal(validate(3), 3);
 
-        assert.throws(() => { validate(1.5) });
-        assert.throws(() => { validate(4) });
-        assert.throws(() => { validate("2") });
-        assert.throws(() => { validate("2.9") });
+            assert.throws(() => { validate(1.5) });
+            assert.throws(() => { validate(4) });
+            assert.throws(() => { validate("2") });
+            assert.throws(() => { validate("2.9") });
 
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate({}) });
-        assert.throws(() => { validate(true) });
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate(true) });
+        });
+
+        it("Coerced", () => {
+            const validate = Validize.createIntegerValidator(1, 3, true);
+            assert.equal(validate(1), 1);
+            assert.equal(validate(3), 3);
+
+            assert.throws(() => { validate(1.5) });
+            assert.throws(() => { validate("2.9") });
+            assert.throws(() => { validate(4) });
+
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate(true) });
+        });
     });
 
-    it("Coerced integer validation", () => {
-        const validate = Validize.createIntegerValidator(1, 3, true);
-        assert.equal(validate(1), 1);
-        assert.equal(validate(3), 3);
+    describe("Float validation", () => {
+        it("Exact", () => {
+            const validate = Validize.createFloatValidator(1, 3);
+            assert.equal(validate(1), 1);
+            assert.equal(validate(1.5), 1.5);
 
-        assert.throws(() => { validate(1.5) });
-        assert.throws(() => { validate("2.9") });
-        assert.throws(() => { validate(4) });
+            assert.throws(() => { validate(4) });
+            assert.throws(() => { validate("2") });
 
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate({}) });
-        assert.throws(() => { validate(true) });
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate(true) });
+        });
+
+        it("Coerced", () => {
+            const validate = Validize.createFloatValidator(1, 3, true);
+            assert.equal(validate(1), 1);
+            assert.equal(validate(1.5), 1.5);
+            assert.equal(validate("2.9"), 2.9);
+            assert.equal(validate("3"), 3);
+            assert.throws(() => { validate(4) });
+
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate(true) });
+        });
     });
 
-    it("Float validation", () => {
-        const validate = Validize.createFloatValidator(1, 3);
-        assert.equal(validate(1), 1);
-        assert.equal(validate(1.5), 1.5);
+    describe("Object validation", () => {
+        it("Empty", () => {
+            const validate = Validize.createValidator<{}>({});
+            assert.deepEqual(validate({}), {});
 
-        assert.throws(() => { validate(4) });
-        assert.throws(() => { validate("2") });
-
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate({}) });
-        assert.throws(() => { validate(true) });
-    });
-
-    it("Coerced float validation", () => {
-        const validate = Validize.createFloatValidator(1, 3, true);
-        assert.equal(validate(1), 1);
-        assert.equal(validate(1.5), 1.5);
-        assert.equal(validate("2.9"), 2.9);
-        assert.equal(validate("3"), 3);
-        assert.throws(() => { validate(4) });
-
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate({}) });
-        assert.throws(() => { validate(true) });
-    });
-
-    it("Object validation (empty)", () => {
-        const validate = Validize.createValidator<{}>({});
-        assert.deepEqual(validate({}), {});
-
-        assert.throws(() => { validate(undefined) });
-        assert.throws(() => { validate(1) });
-        assert.throws(() => { validate("") });
-        assert.throws(() => { validate(true) });
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate(1) });
+            assert.throws(() => { validate("") });
+            assert.throws(() => { validate(true) });
+        });
     });
 
 });
