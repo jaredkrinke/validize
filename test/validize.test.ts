d1800a9f029dcb7b3d835d25c88f48aa0376d35e
Jared Krinke <jared.krinke@gmail.com>
2020-01-05 08:37:48 -0800

    Interface tests

--- test/validize.test.ts
+++ test/validize.test.ts
@@ -1,5 +1,5 @@
 import "mocha";
-import * as assert from "assert";
+import assert from "assert";
 import * as Validize from "../src/validize";
 
 describe("Validize", () => {
@@ -84,6 +84,75 @@ describe("Validize", () => {
             assert.throws(() => { validate(undefined) });
             assert.throws(() => { validate(1) });
             assert.throws(() => { validate("") });
+            assert.throws(() => { validate(true) });
+        });
+
+        it("Interface", () => {
+            interface SomeInterface {
+                i: number;
+                f: number;
+                s: string;
+            }
+
+            const validate = Validize.createValidator<SomeInterface>({
+                i: Validize.createIntegerValidator(1, 3),
+                f: Validize.createFloatValidator(0, 1),
+                s: Validize.createStringValidator(/^[0-9]*$/),
+            });
+            const valid: SomeInterface = {
+                i: 2,
+                f: 0.7,
+                s: "1234",
+            };
+
+            assert.deepEqual(validate(valid), valid);
+
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate({i: 5, s: "123"}) });
+            assert.throws(() => { validate({i: 5, f: 2, s: "123"}) });
+            assert.throws(() => { validate({i: 3, f: 0.5, s: "123", extra: true}) });
+            assert.throws(() => { validate("") });
+            assert.throws(() => { validate(1) });
+            assert.throws(() => { validate(undefined) });
+            assert.throws(() => { validate(true) });
+        });
+
+        it("Nested", () => {
+            interface Inner {
+                i: number;
+                s: string;
+            }
+
+            interface Outer {
+                i: number;
+                inner: Inner;
+            }
+
+            const validate = Validize.createValidator<Outer>({
+                i: Validize.createIntegerValidator(1, 3),
+                inner: Validize.createValidator<Inner>({
+                    i: Validize.createIntegerValidator(1, 10),
+                    s: Validize.createStringValidator(/^[0-9]*$/),
+                })
+            });
+
+            const valid: Outer = {
+                i: 3,
+                inner: {
+                    i: 10,
+                    s: "567",
+                },
+            };
+
+            assert.deepEqual(validate(valid), valid);
+
+            assert.throws(() => { validate({}) });
+            assert.throws(() => { validate({i: 3, inner: {}}) });
+            assert.throws(() => { validate({i: 3, inner: {i: 5}}) });
+            assert.throws(() => { validate({i: 3, inner: {i: 5, s: "a"}}) });
+            assert.throws(() => { validate("") });
+            assert.throws(() => { validate(1) });
+            assert.throws(() => { validate(undefined) });
             assert.throws(() => { validate(true) });
         });
     });
